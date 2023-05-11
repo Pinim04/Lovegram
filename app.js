e19219915ce1ac09df0198081bba22e25a33290c
MattiaBorali <boralim17@itisvinci.com>
2023-05-11 19:10:15 +0200

    creato login

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ liveReloadServer.server.once("connection", () => {
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/login');
 
 var app = express();
 
@@ -35,6 +36,7 @@ app.use(connectLiveReload());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/login', loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
