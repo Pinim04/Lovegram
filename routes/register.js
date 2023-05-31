6b275c5e1727abe71ed29177e6c4f2a9bac2fab0
leocove21 <covezzolil2016@itisvinci.com>
2023-05-31 20:39:06 +0200

    query di controllo e inserimento utente

--- routes/register.js
+++ routes/register.js
@@ -6,10 +6,17 @@ router.get('/', function(req, res, next) {
   }
 );
 
-// router.post('/', function(req, res, next) {
-//   const username = req.body.usr;
-//   req.session.usr = username;
-//   res.redirect('/home');
-// });
+ router.post('/insert', function(req, res, next) {
+  const qCtrl = 'SELECT * FROM Utente WHERE Username = "leocove21";'
+  const searchedName = form.name;
+  if(queryCtrl == insertedName)
+  {
+    console.write("Utente già presente, si prega di inserire un username differente!");
+  }
+  else
+  {
+    const insertName = "INSERT INTO Utente (Username, Email, Pwd, Nome, Cognome, Sesso, Ddn, IdFoto, IdCitta) VALUES ('nuovo_utente', 'nuovo_utente@example.com', 'password123', 'Nuovo', 'Utente', 'Altro', '1990-01-01', 1, 1);"
+  }
+});
 
 module.exports = router;
