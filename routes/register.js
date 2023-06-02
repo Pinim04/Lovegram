af3c438c437a900710b5b5167fe25678c1abb5e2
leocove21 <covezzolil2016@itisvinci.com>
2023-06-02 20:32:44 +0200

    tentativo salvataggio foto media

--- routes/register.js
+++ routes/register.js
@@ -1,37 +1,12 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-    res.render('register', { title: 'Registrati' });
-  }
-);
-
- router.post('/insert', function(req, res, next) {
-  const mysql = require('mysql');
-
-  const connection = mysql.createConnection({
-    host: 3000,
-    user: "TheRealCovez",
-    password: "Leocove21",
-    database: "Lovegram"
-  });
-
-  app.post('/submit', (req, res) => {
-    const username = req.body.username;
-
-    const qInsert = "INSERT INTO Utente (Username) VALUES ('${username}')";
-    
-    connection.qInsert(qInsert, (error, results) => {
-      if(error) {
-        console.error('Error inserting user: ', error);
-        return res.status(500).send('Error inserting user');
-      }
-
-      return res.status(200).send('User insert successfully');
-    });
-  });
-
-  
+router.get("/", function (req, res, next) {
+  res.render("register", { title: "Registrati" });
 });
+
+const fs = require("fs");
+
+router.post("/", (req, res) => {});
 
 module.exports = router;
