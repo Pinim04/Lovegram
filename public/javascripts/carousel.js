02ba1f35d748de2c6685168538edfd0e2c9bacee
Matteo Pini <pinim.202@gmail.com>
2023-06-02 14:23:46 +0200

    carosel basics

---
+++ public/javascripts/carousel.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $(".carousel").slick({
+    centerMode: true,
+    centerPadding: "60px",
+    slidesToShow: 3,
+    arrows: false,
+    infinite: true
+  });
+});
