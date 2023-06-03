34c4581e5b1f4a2087b1201c6c8e24c29a4a9d08
pinim.202@gmail.com <pinim.202@gmail.com>
2023-06-03 07:14:43 +0200

    more or less

--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,20 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const fs = require("fs");
+const path = require("path");
 
-router.get('/', function(req, res, next) {
-  res.render('home', {title: "ciao"});
+router.get("/", function (req, res, next) {
+  // Read the contents of the media folder
+  const mediaFolder = path.join(__dirname, "../public", "Images", "media");
+  fs.readdir(mediaFolder, (err, files) => {
+    if (err) {
+      console.error("Error reading media folder:", err);
+      return res.status(500).send("Internal Server Error");
+    }
+
+    // Pass the list of image filenames to the EJS template
+    res.render("home", { images: files });
+  });
 });
 
 module.exports = router;
